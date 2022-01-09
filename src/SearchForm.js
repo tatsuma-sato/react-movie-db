792b3c3dcf66d1dd6099b644219ebf907728d39b
tatsuma-sato <t.tsuma.s@gmail.com>
2022-01-08 19:17:01 -0800

    added features

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,7 +1,24 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 const SearchForm = () => {
-  return <h2>search component</h2>
-}
+  const { query, setQuery, error } = useGlobalContext();
 
-export default SearchForm
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className="search-form">
+      <h2>Search movies</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={handleChange}
+      />
+      {query !== "" && error.show && <div className="error">{error.msg}</div>}
+    </form>
+  );
+};
+
+export default SearchForm;
--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,9 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { API_ENDPOINT } from './context'
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { API_ENDPOINT } from "./context";
+import useFetch from "./useFetch";
+
+const url =
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const SingleMovie = () => {
-  return <h2>single movie</h2>
-}
+  const { id } = useParams();
 
-export default SingleMovie
+  const { isLoading, error, data: movie } = useFetch(`&i=${id}`);
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  if (error.show) {
+    return (
+      <div className="page-error">
+        <h1>{error.msg}</h1>
+        <Link to="/" className="btn">
+          back to movies
+        </Link>
+      </div>
+    );
+  }
+
+  const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
+
+  return (
+    <section className="single-movie">
+      <img src={poster === "N/A" ? url : poster} alt={title} />
+      <div className="single-movie-info">
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+      </div>
+    </section>
+  );
+};
+
+export default SingleMovie;
